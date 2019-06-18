f73b7fe5d736cb1632b9a17226a7e7becbf3249a
Chester Zara <chesterjzara@gmail.com>
2019-06-18 11:20:32 -0500

    Added places routes to add new place/review

--- controllers/places_controller.js
+++ controllers/places_controller.js
@@ -23,7 +23,7 @@ router.get('/tags', async (req, res) => {
 
         let optionResults = rows.map( (item) => {
             return (
-                { value: item.tag_name, label: capitalizeFirstLetter(item.tag_name)}
+                { value: item.tag_id, label: capitalizeFirstLetter(item.tag_name)}
             )
         })
         // console.log(rows)
@@ -40,7 +40,50 @@ router.get('/tags', async (req, res) => {
 router.post('/new', VerifyToken, async (req, res) => { 
     //Add code to save a new place here
     let current_user_id = req.user_id
-    const {place_id, address, name, google_url, tag, new_tag, review_text } = req.body
+    const {place_id, address, name, google_url, tag, new_tag, review_text, rating } = req.body
+
+    try {
+        // If this is a new_tag, the tag is a string value
+            // Else, it is a int tag_id value
+        let tag_id
+        if (new_tag === true) {
+            const tagResults = await db.query(`
+                INSERT INTO tags (tag_name)
+                VALUES ($1)
+                RETURNING tag_id; 
+            `, [tag])
+            tag_id = tagResults.rows[0].tag_id
+        } else {
+            tag_id = tag
+        }
+        // Store the place info in the 'users_places' table
+        const placesResults = await db.query(`
+            INSERT INTO users_places (user_id, place_name, place_id, address, google_url, date_added, rating, review)
+            VALUES ($1, $2, $3, $4, $5, current_date, $6, $7)
+            RETURNING entry_id, user_id, place_name, place_id, address, google_url, date_added, rating, review;
+        `, [current_user_id, name, place_id, address, google_url, rating, review_text])
+        // Save the new entry_id to associate with the tag(s)
+        let stored_entry_id = placesResults.rows[0].entry_id
+
+        
+        const placeTagResults = await db.query(`
+            INSERT INTO user_places_tags (entry_id, tag_id)
+            VALUES ($1, $2)
+            RETURNING id, entry_id, tag_id
+        `, [stored_entry_id, tag_id])
+
+        res.status(200).json({
+            place: placesResults.rows[0],
+            tag: placeTagResults
+        })
+    
+    } catch(e) {
+        console.log(e)
+        res.status(400).json({
+            status: 400,
+            message: 'An error occurred, try again.'
+        })
+    }
 
     // If we have newTag = true
         // Store the new tag in the 'tags' table, return the new id number
