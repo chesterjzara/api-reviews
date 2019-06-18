e13d79908ee04212480c89c88a55f1a43ae425bc
Chester Zara <chesterjzara@gmail.com>
2019-06-17 21:58:45 -0500

    Tag retreival method and start of saving a place/review

--- controllers/places_controller.js
+++ controllers/places_controller.js
@@ -12,3 +12,48 @@ module.exports = router;
 //     let user_id = req.user_id
 
 // })
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+router.get('/tags', async (req, res) => { 
+    try {
+        const {rows} = await db.query('SELECT * FROM tags;')
+
+        let optionResults = rows.map( (item) => {
+            return (
+                { value: item.tag_name, label: capitalizeFirstLetter(item.tag_name)}
+            )
+        })
+        // console.log(rows)
+        // console.log(optionResults)
+        res.status(200).json(optionResults)
+    } catch(e){
+        res.status(400).json({
+            status: 400,
+            message: 'An error occurred, try again.'
+        })
+    }
+})
+
+router.post('/new', VerifyToken, async (req, res) => { 
+    //Add code to save a new place here
+    let current_user_id = req.user_id
+    const {place_id, address, name, google_url, tag, new_tag, review_text } = req.body
+
+    // If we have newTag = true
+        // Store the new tag in the 'tags' table, return the new id number
+    // Else ]
+        // Lookup the number for the tag
+    
+    // Store the rest of the information in the places tab
+    // Store info in the user_places table
+    // Store the tag info in the user_places_tags table
+
+})
+
+// // const options = [
+//     { value: 'chocolate', label: 'Chocolate' },
+//     { value: 'strawberry', label: 'Strawberry' },
+//     { value: 'vanilla', label: 'Vanilla' }
